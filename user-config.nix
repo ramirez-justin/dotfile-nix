35a5d439111a42eff55437eb52a1b9933b9205f1
Justin Ramirez <95737913+ramirez-justin@users.noreply.github.com>
2025-03-12 18:38:53 -0400

    untrack user-config.nix

--- user-config.nix
+++
@@ -1,9 +0,0 @@
-# Copy this file to user-config.nix and update with your information
-# Note: 'username' must match your macOS username (the one you use to log in)
-{
-  username = "satyasheel";
-  fullName = "Satyasheel";
-  email = "satyasheel@lightricks.com";
-  githubUsername = "mlgruby";
-  hostname = "satyasheel-MBP";
-} 
