61e225038f8c0d435429b9a32159255a67ef789b
jram <95737913+ramirez-justin@users.noreply.github.com>
2025-12-05 23:38:37 -0500

    fix error and deprecation

--- home-manager/modules/gcloud.nix
+++ home-manager/modules/gcloud.nix
@@ -52,7 +52,7 @@ in
   home.packages = [ gdk ];
 
   programs.zsh = {
-    initExtra = ''
+    initContent = ''
       # Initialize Google Cloud SDK
       # Adds completions and updates PATH
       # Source: google-cloud-sdk package from nixpkgs
